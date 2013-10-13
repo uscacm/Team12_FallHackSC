4f265c723d608c0781fe1e812dcc5d1bbf76d84e
Kalyanaraman Santhanam <kalyan2s2@gmail.com>
2013-10-13 12:24:25 -0700

    facebook basic

--- workspaceAndroidHack/AndroidApp/src/com/example/androidapp/LoginActivity.java
+++ workspaceAndroidHack/AndroidApp/src/com/example/androidapp/LoginActivity.java
@@ -1,12 +1,20 @@
 package com.example.androidapp;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 import android.animation.Animator;
 import android.animation.AnimatorListenerAdapter;
 import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Intent;
+import android.content.pm.PackageInfo;
+import android.content.pm.PackageManager;
+import android.content.pm.PackageManager.NameNotFoundException;
+import android.content.pm.Signature;
 import android.os.Build;
 import android.os.Bundle;
 import android.text.TextUtils;
+import android.util.Base64;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.Menu;
@@ -21,6 +29,10 @@ import com.parse.ParseAnalytics;
 import com.parse.ParseException;
 import com.parse.ParseUser;
 import com.parse.SignUpCallback;
+
+
+import com.facebook.*;
+import com.facebook.model.*;
 
 /**
  * Activity which displays a login screen to the user, offering registration as
@@ -46,9 +58,10 @@ public class LoginActivity extends Activity {
 	// UI references.
 	private EditText mEmailView;
 	private EditText mPasswordView;
+	private EditText mConfirmPasswordView;
 	private View mLoginFormView;
 	private View mLoginStatusView;
-	private TextView mLoginStatusMessageView, mErrorMessageView;
+	private TextView mLoginStatusMessageView, mErrorMessageView, mSuccessView;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
@@ -62,9 +75,31 @@ public class LoginActivity extends Activity {
 		mEmail = getIntent().getStringExtra(EXTRA_EMAIL);
 		mEmailView = (EditText) findViewById(R.id.email);
 		mEmailView.setText(mEmail);
-		
+
 		mErrorMessageView = (TextView) findViewById(R.id.errorMessageTextView);
 		mErrorMessageView.setText("");
+		
+		mSuccessView = (TextView) findViewById(R.id.mSuccessView);
+		mSuccessView.setText("");
+		
+		mConfirmPasswordView = (EditText) findViewById(R.id.confirmpassword);
+		mConfirmPasswordView.setVisibility(View.INVISIBLE);
+		// Add code to print out the key hash
+		try {
+			PackageInfo info = getPackageManager().getPackageInfo(
+					"com.example.androidapp", 
+					PackageManager.GET_SIGNATURES);
+			for (Signature signature : info.signatures) {
+				MessageDigest md = MessageDigest.getInstance("SHA");
+				md.update(signature.toByteArray());
+				Log.d(LOG, Base64.encodeToString(md.digest(), Base64.DEFAULT));
+				mErrorMessageView.setText(Base64.encodeToString(md.digest(), Base64.DEFAULT));
+			}
+		} catch (NameNotFoundException e) {
+			Log.e(LOG, e.getMessage());
+		} catch (NoSuchAlgorithmException e) {
+			Log.e(LOG, e.getMessage());
+		}
 
 		mPasswordView = (EditText) findViewById(R.id.password);
 		mPasswordView
@@ -85,12 +120,58 @@ public class LoginActivity extends Activity {
 		mLoginStatusMessageView = (TextView) findViewById(R.id.login_status_message);
 
 		findViewById(R.id.sign_in_button).setOnClickListener(
-			new View.OnClickListener() {
-				@Override
-				public void onClick(View view) {
-					attemptLogin();
-				}
+				new View.OnClickListener() {
+					@Override
+					public void onClick(View view) {
+						attemptLogin();
+					}
+				});
+
+		findViewById(R.id.facebook_button).setOnClickListener(
+				new View.OnClickListener() {
+					@Override
+					public void onClick(View view) {
+						facebookLogin();
+					}
+				});
+		
+	}
+
+	
+	private void facebookLogin()
+	{
+		Session.openActiveSession(this, true, new Session.StatusCallback() {
+			
+			// callback when session changes state
+			@Override
+			public void call(Session session, SessionState state, Exception exception) {
+				if (session.isOpened()) {
+
+			          // make request to the /me API
+			          Request.executeMeRequestAsync(session, new Request.GraphUserCallback() {
+
+			            // callback after Graph API response with user object
+			            @Override
+			            public void onCompleted(GraphUser user, Response response) {
+			              if (user != null) {
+			            	  mSuccessView.setText("Hello " + user.getName() + "!");
+			              }
+			              else
+			              {
+			            	  mSuccessView.setText("Errpr" + response.toString());
+			              }
+			            }
+			          });
+			        }
+			      }
 		});
+		
+		
+	}
+	@Override
+	public void onActivityResult(int requestCode, int resultCode, Intent data) {
+		super.onActivityResult(requestCode, resultCode, data);
+		Session.getActiveSession().onActivityResult(this, requestCode, resultCode, data);
 	}
 
 	@Override
@@ -146,7 +227,7 @@ public class LoginActivity extends Activity {
 		} else {
 			// Show a progress spinner, and kick off a background task to
 			// perform the user login attempt.
-			mLoginStatusMessageView.setText(R.string.login_progress_signing_in);
+			mLoginStatusMessageView.setText(R.string.progress_signing_in);
 			showProgress(true);
 
 			ParseUser.logInInBackground(mEmail, mPassword, new LogInCallback() {
@@ -189,7 +270,7 @@ public class LoginActivity extends Activity {
 								}
 							}
 						});
-						
+
 					}
 				}
 			});
